26d54733ae0cbc689c9ff550622958b1b82e22a7
elaps <elaps@bk.ru>
2024-07-02 21:01:49 +0500

    first

---
+++ web/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+include '../app/vendor/autoload.php';
+
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    Ну привет
+    </body>
+</html>
